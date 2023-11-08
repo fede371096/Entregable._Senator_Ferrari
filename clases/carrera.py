3cf756d8e7ccfdf1c12a28d5752dcd3219f2db3f
Federico Ferrari <federicoferraririus@gmail.com>
2023-11-08 20:20:00 -0300

    Se configuro la asignacion de puntos en una carrera

--- clases/carrera.py
+++ clases/carrera.py
@@ -47,8 +47,7 @@ class Carrera:
         self.__pilotos = pilotos
         
     def vincular_equipo(self, *equipo):
-        for temp_equipo in equipo:
-            self.equipos.append(temp_equipo)
+        self.equipos.append(equipo)
         
     def ingresar_pilotos(self):
         for lista in self.equipos:
@@ -82,7 +81,7 @@ class Carrera:
                             self.penalidades.append(numero)
 
     def obtener_equipo(self,numero_de_auto):
-        for lista in numero_de_auto:
+        for lista in self.equipos:
             for equipo in lista:
                 for piloto in equipo.pilotos:
                     if piloto.numero_de_auto == numero_de_auto:
@@ -96,20 +95,45 @@ class Carrera:
     
     def habilidad_auto(self,equipo):
         suma_de_habilidad = 0
-        for auto in equipo.auto:
-            suma_de_habilidad += auto.habilidad
+        for temp_equipo in self.equipos:
+            if equipo == temp_equipo:
+                suma_de_habilidad += equipo.auto.habilidad
         return suma_de_habilidad
+    
+    def asignar_puntos(self):
+        for piloto in self.pilotos:
+            if piloto == self.pilotos[0]:
+                piloto.puntos += 25
+            elif piloto == self.pilotos[1]:
+                piloto.puntos += 18
+            elif piloto == self.pilotos[2]:
+                piloto.puntos += 15
+            elif piloto == self.pilotos[3]:
+                piloto.puntos += 12
+            elif piloto == self.pilotos[4]:
+                piloto.puntos += 10
+            elif piloto == self.pilotos[5]:
+                piloto.puntos += 8
+            elif piloto == self.pilotos[6]:
+                piloto.puntos += 6
+            elif piloto == self.pilotos[7]:
+                piloto.puntos += 4
+            elif piloto == self.pilotos[8]:
+                piloto.puntos += 2
+            elif piloto == self.pilotos[9]:
+                piloto.puntos += 1
     
     def puntos_totales(self):
         for piloto in self.pilotos:
-            piloto.puntos = 0
+            piloto.habilidad_total = 0
             if piloto in self.abandonos:
-                piloto.puntos = 0
+                piloto.puntos += 0
             else:
                 errores = self.errores_en_pits.count(piloto)
                 penalidad = self.penalidades.count(piloto)
                 equipo = self.obtener_equipo(piloto.numero)
                 piloto.habilidad_total = self.habilidad_mecanicos(equipo) + self.habilidad_auto(equipo) + piloto.habilidad - (errores*5) - (penalidad*8)
-        self.pilotos.sort(key=lambda piloto:piloto[-1])
+            self.pilotos.sort(key=lambda piloto:piloto[-1])
+        self.asignar_puntos()
         
 
